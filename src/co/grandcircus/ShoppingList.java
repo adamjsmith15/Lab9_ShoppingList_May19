5b8d05d5eac3966a373f84a2b2908935ca9dcdd3
adamjsmith15 <ajsmith015@gmail.com>
2019-05-01 19:15:03 -0400

    Second commit for Shopping List Lab 9, completed challenges

--- src/co/grandcircus/ShoppingList.java
+++ src/co/grandcircus/ShoppingList.java
@@ -1,6 +1,7 @@
 package co.grandcircus;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Scanner;
@@ -15,25 +16,28 @@ public class ShoppingList {
 		List<String> marketItems = createMarketItems();
 		List<Double> itemPrice = createItemPrice();
 		List<String> itemsInCart = new ArrayList<>();
-		List<Double> pricesInCart =  new ArrayList<>();
+		List<Double> pricesInCart = new ArrayList<>();
+		List<Integer> quantityInCart = createCart();
 		
-		HashMap<String, Double> shoppingItems = new HashMap<>();
-		
-		shoppingItems = createHashMap(marketItems, itemPrice);
+
+		//HashMap<String, Double> shoppingItems = new HashMap<>();
+
+		//shoppingItems = createHashMap(marketItems, itemPrice);
 
 		while (stillShopping == true) {
 			displayMenu(marketItems, itemPrice);
 			addSpacing(2);
 			String userInput = Validator.getString(scnr, "What item would you like to order?  ");
 			userInput = checkUserInput(userInput, marketItems);
-			
-			addToCart(userInput, itemsInCart, pricesInCart, marketItems, itemPrice);
-			
+
+			addToCart(userInput, itemsInCart, pricesInCart, marketItems, itemPrice, quantityInCart);
+			System.out.println(quantityInCart);
+
 			stillShopping = continueShopping(scnr, "Would you like to order anything else (y/n)? ");
 			addSpacing(1);
 
 		}
-		printCart(itemsInCart, pricesInCart);
+		printCart(itemsInCart, pricesInCart, quantityInCart, itemPrice);
 		System.out.println(maxItem(itemsInCart, pricesInCart));
 		System.out.println(minItem(itemsInCart, pricesInCart));
 
@@ -49,7 +53,7 @@ public class ShoppingList {
 		int counter = 0;
 		for (String item : items) {
 			// double itemPrice = (double)price[items.indexOf(item)];
-			System.out.printf("%d %-20s %-20s \n", counter+1, item, price.get(counter));
+			System.out.printf("%d %-20s %-20s \n", counter + 1, item, price.get(counter));
 			counter++;
 
 		}
@@ -85,6 +89,11 @@ public class ShoppingList {
 		return itemPrice;
 	}
 
+	public static List<Integer> createCart() {
+		List<Integer> cart = new ArrayList<>(Collections.nCopies(8, 0));
+		return cart;
+	}
+
 	public static boolean checkMenu(String userOrder, List<String> arr, List<Double> price) {
 		if (arr.contains(userOrder)) {
 			System.out.println("Adding " + userOrder + " to cart at " + price.get(arr.indexOf(userOrder)));
@@ -95,47 +104,55 @@ public class ShoppingList {
 		}
 
 	}
-	
-	public static HashMap<String, Double> createHashMap(List<String> arr, List<Double> price){
+
+	public static HashMap<String, Double> createHashMap(List<String> arr, List<Double> price) {
 
 		HashMap<String, Double> map = new HashMap<>();
-		for(int i = 0; i < arr.size(); i++) {
+		for (int i = 0; i < arr.size(); i++) {
 			map.put(arr.get(i), price.get(i));
 		}
 		return map;
 	}
-	
+
 	public static String checkUserInput(String userInput, List<String> arr) {
 		try {
-		int num = Integer.parseInt(userInput);
-		return arr.get(num - 1);
-		}catch (Exception e) {
+			int num = Integer.parseInt(userInput);
+			return arr.get(num - 1);
+		} catch (Exception e) {
 			return userInput;
 		}
 	}
-	
-	public static void addToCart(String userOrder, List<String> cartItems, List<Double> cartPrice, List<String> items, List<Double> price) {
-		if(checkMenu(userOrder, items, price)) {
-			cartItems.add(userOrder);
-			cartPrice.add(price.get(items.indexOf(userOrder)));
-		}else {
+
+	public static void addToCart(String userOrder, List<String> cartItems, List<Double> cartPrice, List<String> items,
+			List<Double> price, List<Integer> quantity) {
+		if (checkMenu(userOrder, items, price)) {
+			if (cartItems.contains(userOrder)) {
+				//quantity.add(items.indexOf(userOrder), quantity.get(items.indexOf(userOrder)) + 1);
+				quantity.set(items.indexOf(userOrder), quantity.get(items.indexOf(userOrder)) + 1);
+			} else {
+				//quantity.add(items.indexOf(userOrder), quantity.get(items.indexOf(userOrder)) + 1);
+				quantity.set(items.indexOf(userOrder), quantity.get(items.indexOf(userOrder)) + 1);
+				cartItems.add(userOrder);
+				cartPrice.add(price.get(items.indexOf(userOrder)));
+			}
+		} else {
 			System.out.println("Sorry, we dont' have those. Please try again. ");
 			addSpacing(1);
 		}
-		
+
 	}
-	
-	public static void printCart(List<String> cartItems, List<Double> cartPrice) {
-		double sum = 0;
+
+	public static void printCart(List<String> cartItems, List<Double> cartPrice, List<Integer> quantity, List<Double> price) {
 		System.out.println("Thanks for your order!");
 		System.out.println("Here's what you got:");
-		for(int i = 0; i < cartItems.size(); i++) {
-			System.out.printf("%-15s %-15s \n", cartItems.get(i), cartPrice.get(i));
-			sum += cartPrice.get(i);
+		for (int i = 0; i < cartItems.size(); i++) {
+			System.out.printf("%-15s %-10s %d \n", cartItems.get(i), cartPrice.get(i), quantity.get(price.indexOf(cartPrice.get(i))));
 		}
-		
+		addSpacing(1);
+
 		System.out.print("Average price per item in order was ");
-		System.out.printf("%.2f \n", + sum/cartPrice.size());
+		System.out.printf("%.2f \n", averagePrice(price, quantity));
+		addSpacing(1);
 	}
 
 	public static boolean continueShopping(Scanner scnr, String promp) {
@@ -147,31 +164,45 @@ public class ShoppingList {
 			return false;
 		}
 	}
-	
+
 	public static String maxItem(List<String> cartItems, List<Double> cartPrice) {
 		double max = 0;
 		for (int i = 0; i < cartItems.size(); i++) {
-			if(max > cartPrice.get(i)) {
+			if (max > cartPrice.get(i)) {
 				continue;
-			}else {
+			} else {
 				max = cartPrice.get(i);
 			}
 		}
 		int indexOfMax = cartPrice.indexOf(max);
-		return "The most expensive item is " + cartItems.get(indexOfMax) + " at a price of " + cartPrice.get(indexOfMax) + ".";
+		return "The most expensive item is " + cartItems.get(indexOfMax) + " at a price of " + cartPrice.get(indexOfMax)
+				+ ".";
 	}
-	
+
 	public static String minItem(List<String> cartItems, List<Double> cartPrice) {
+
 		double min = 5;
 		for (int i = 0; i < cartItems.size(); i++) {
-			if(min < cartPrice.get(i)) {
+			if (min < cartPrice.get(i)) {
 				continue;
-			}else {
+			} else {
 				min = cartPrice.get(i);
 			}
 		}
 		int indexOfMin = cartPrice.indexOf(min);
-		return "The lease expensive item is " + cartItems.get(indexOfMin) + " at a price of " + cartPrice.get(indexOfMin) + ".";
+		return "The lease expensive item is " + cartItems.get(indexOfMin) + " at a price of "
+				+ cartPrice.get(indexOfMin) + ".";
+	}
+	
+	public static Double averagePrice(List<Double> price, List<Integer> quantity) {
+		double sum = 0;
+		int div = 0;
+		for(int i = 0; i < quantity.size(); i++) {
+			sum = sum + price.get(i) * quantity.get(i);
+			div += quantity.get(i);
+		}
+		
+		return sum/div;
 	}
 
 	public static void addSpacing(int num) {
